5f2492044399e69ad1b720c458b12fc5df689690
Gonçalo Silva <gmrs99.silva@gmail.com>
2021-07-07 19:53:23 +0100

    Criado o layout para inserir um novo utente

--- app/src/main/java/pt/ipg/covidapp/NovoUtenteFragment.kt
+++ app/src/main/java/pt/ipg/covidapp/NovoUtenteFragment.kt
@@ -16,7 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Novo_Utente_Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Novo_Utente_Fragment : Fragment() {
+class NovoUtenteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,7 +49,7 @@ class Novo_Utente_Fragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Novo_Utente_Fragment().apply {
+            NovoUtenteFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
